e316f498d096209bd973ce874ea1de88e9516bda
Alexandr <babenkoalexandr06@gmail.com>
2024-10-17 19:19:39 +0300

    Documentation for: rectangle.py, circle.py, square.py

--- circle.py
+++ circle.py
@@ -2,9 +2,20 @@ import math
 
 
 def area(r):
+    '''Принимает число r - радиус круга, возвращает его площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
-    return 2 * math.pi * r
+    '''
+    Возвращает переметр круга.
+
+        Параметры:
+            r (int) : Радиус круга.
+        
+        Возвращаемое значение:
+            per (int) : Периметр круга.
+    '''
+    per = 2 * math.pi * r
+    return per
 
